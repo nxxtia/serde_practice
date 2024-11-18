dab0922604fe1c43104cdc81701e9036e13ec35b
nxxtia <126991328+nxxtia@users.noreply.github.com>
2024-11-18 18:45:23 +0200

    Update main.rs

--- serde_practice/src/main.rs
+++ serde_practice/src/main.rs
@@ -1,87 +1,38 @@
 use serde::{Serialize, Deserializer, Deserialize, Serializer};
-use serde_json;
-
-use std::fs::File;
-use std::io::Read;
-
-use toml::to_string as to_toml;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct PublicTariff {
-    id: u32,
-    price: u32,
-    duration: String,
-    description: String,
+struct Event {
+    name: String,
+    #[serde(
+        serialize_with = "serialize_date",
+        deserialize_with = "deserialize_date"
+    )]
+    date: String,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct PrivateTariff {
-    client_price: u32,
-    duration: String,
-    description: String,
+fn serialize_date<S: Serializer>(date: &str, serializer: S) -> Result<S::Ok, S::Error>
+{
+    serializer.serialize_str(&format!("This is date: {}", date))
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Stream {
-    user_id: String,
-    is_private: bool,
-    settings: u32,
-    shared_url: String,
-    public_tariff: PublicTariff,
-    private_tariff: PrivateTariff,
+fn deserialize_date<'d, D: Deserializer<'d>>(deserializer: D) -> Result<String, D::Error>
+{
+    let s: &str = Deserialize::deserialize(deserializer)?;
+    Ok(s.replace("date", "event date"))
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-enum RequestType{
-    success,
-}
+fn main() {
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Gift {
-    id: u32,
-    price: u32,
-    description: String,
+    let event = Event {
+        name: String::from("Circus"),
+        date: String::from("2024-14-11")
+    };
 
-}
+    let json = serde_json::to_string(&event).expect("error");
+    dbg!(&json);
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Debug {
-    duration: String,
-    at: String,
+    let deserialized_json: Event = serde_json::from_str(&json).expect("error");
 
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Request {
-    r#type: RequestType,
-    stream: Stream,
-    gifts: Vec<Gift>,
-    debug: Debug
-}
-
-fn main() -> Result<(), serde_json::Error> {
-
-    let mut file = File::open("/Users/nastyamillow/Desktop/Rust/serde_practice_/serde_practice/src/request.json").unwrap();
-    let mut json_str = String::new();
-    file.read_to_string(&mut json_str).unwrap();
-
-    let request: Request = serde_json::from_str(&json_str).unwrap();
-
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let mut file = File::open("/Users/nastyamillow/Desktop/Rust/serde_practice_/serde_practice/src/request.json").unwrap();
-        let mut json_str = String::new();
-        file.read_to_string(&mut json_str).unwrap();
-    
-        let request: Request = serde_json::from_str(&json_str).unwrap();
-
-        assert_eq!(request.stream.public_tariff.price, 100);
-    }
+    dbg!(json);
+    dbg!(deserialized_json);
 }
